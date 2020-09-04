77f36a554c48b8b35c82e90ddefda39484fe8ef8
D-p-a-k <67870389+D-p-a-k@users.noreply.github.com>
2020-09-04 21:10:45 +0530

    Add files via upload

---
+++ photo/data.js
@@ -0,0 +1,86 @@
+var data = [];
+
+var dataStr = "1.prasanth anna<br>\
+<br>\
+Avanga ellam photo laye sollitangalam pa inga onnum illa<br>\
+<br>\
+<br>\
+2.hari anna<br>\
+<br>\
+Iva Ragaviii ila Raagav &#129315 Apopo ithu ponna enanu oru doubtu vanthute irukum ba..<br>Anti-Fam in Lady President (Indru mudhal) &#129325 Sat-sunday vanthuta pothum crcta call paniruvanga baa...<br>Game viladumnu sonna ORGANISER Puliya maaridum...Enga kootathule mokkana athu Hp dha.. Aana avaruke tough kudukrathunaa athu namma productu dhaan.. Mothathula patha iva oru<br> MUZHU-PAITHIYUM..<br>Aappie birthday Raagav &#10084  &#65039 <br>\
+<br>\
+<br>\
+3.madhu anna<br>\
+<br>\
+Japanese Cartoon<br>\
+<br>\
+<br>\
+4.DP<br>\
+<br>\
+Wishing u the best in everything boo...Have an awesome bithday!<br>\
+<br>\
+<br>\
+5.nive<br>\
+<br>\
+Hey raaggggssss...Namma 1st nss meet la thaan meet pannom       &#128513 1st impression...Friendly &#128151 Aparam leo club classes la unna paathuruken, then some meets...Apadiye konjam konjam ah pesa aaramichom &#128523..You are a bold and daring person &#128526 Your dedication in doing any task amazes me  And you always remain very true to people around you &#128151 That is something which I like the most in you &#128536. Lovely character &#128525...Stay the same raaggggssss &#128151 &#128151 Innum neraiya time namma onna spend pannanum &#128145..Wishing you a very happy birthday &#128525 May all your dreams come true &#128151 Have a wonderful year ahead &#129321<br>\
+<br>\
+<br>\
+6.keets<br>\
+<br>\
+One of the best frnd I got in nss....jovial funny crazy &#128514 &#128151 and bayangra book reader ....such a cute and caring girl...avakuda pesuna it feels like we were been frnds for almost a decade &#128514 &#x2728...talented one ..and lovely one...frankly and from my I heart I say 'I love her more than other nss frnds in girls' &#128151 ...thuru thuru nu irupa...no attitude &#128525 ...and atlast my wish to you babe - Happiest bday crazy reader &#128514 &#x2728<br>\
+<br>\
+<br>\
+7.Its me your thollai<br>\
+<br>\
+Enna solrathu....You are my guide for all.ennoda akka ku marraige aana aprm romba miss pannen adha ne than pokuna.you are a wonderful sister. Indha bday la irunthu ellam nallathavae nadakkum...ennoda thollaigal thavara...be happy.<br> HAPPY BIRTHDAY SISTER!!!<br>\
+<br>\
+<br>\
+8.sunil<br>\
+<br>\
+amaithiyana ponnu &#128556;..yaaru vambukum koopta kuda poga maata &#128514;...Have a great yr ahead..iniya piranthanaal nalvaalthugal.<br>\
+<br>\
+<br>\
+9.sangavi<br>\
+<br>\
+One of the best souls &#10084 &#65039...enoda Xerox &#129321...nama spend pana time konjama irunthalum....it really made great happiness when we all together &#129392 ....Ipo irukra maathiriye epothum fun ah jolly ah sirichite irunga &#128513...Wishing you a great great success to achieve your goal &#128165... stay the same ka &#129392... Stay happy forever &#129303... Nd finally....<br> &#128155 Wish you many more happy returns of the day Ragavi akka! &#128155<br>\
+<br>\
+<br>\
+10.harini<br>\
+<br>\
+Happy happyyy birthday T-snr &#10084 &#65039..designer &#128293.. 'weird' oda human representaion neenga &#129322...paka matum 'indha poonayu pal kudikuma' nu irukradhu &#128518..series paithiyam &#128565..photo nu sonna podhu modha aala poyi ninukradhu &#128586..for your info, place anadhuku trt kudukala inu &#129317..Apapo enayu padichiya nu kepinga &#128534..enoda polambal la ketu epavu siripinga &#129301..aana ela project kum edachu idea kuduthu udhavum jeevan &#128526<br>\
+<br>\
+<br>\
+11.manoj<br>\
+<br>\
+Happy Quarantine Birthday Rags &#128293 &#129395 &#10084 &#65039 !! Lot of funn and beautiful memories with u &#129315 &#10083 &#65039 and helped me more during placements &#10083 &#65039 &#128525  series partner &#128514 &#128514 place anathuku pona birthday ku ipo intha birthday ku ellathuku serthu treat vaikura nee &#129488 &#129488 and give me that dairy milk idiot &#128530 ....Have a wonderful and healthy year ahead &#10083 &#65039 &#128525 be happy foreveeerrrr &#128293 &#128522 &#129303 &#129303<br>\
+<br>\
+<br>\
+12.pandi<br>\
+<br>\
+Ragavi ennoda Nss friend &#128525 &#128525 &#128525 ..entha help naalum vudane pannuva &#128541 &#128541 ....romba dedication ana girl &#128514 ...social responsibility person &#128522 ..oru varusam ma treat vaikkuren nu engala lam nalla mottai pottu ramatha thadavitta &#128541 &#128541 &#129315 &#129315<br>\
+<br>\
+<br>\
+13.vimal<br>\
+<br>\
+So basically rags u are my chess student no matter how well u play .. it all started in our unit ...that day when we were at the school. U never fail to give me hope whenever I feel down.. u had the trust in me and shared lot many things which made me feel so blessed to have a person like u beside . Despite of all the chaos u understood me and listened to me and explained things in a much better way. One good soul that I had come across in my entire life &#10084 &#65039 .. someone I'll never forget ... That's u!<br>\
+<br>\
+<br>\
+14.Thenmozhi<br>\
+<br>\
+Hii Ragavi! &#10084 &#65039 NSS la kidaicha oru nalla uravu.We have a lot of gud times&#128105 &zwj &#10084 &#65039 &zwj &#128105 together.First patha apo eppadi irunthayo athe maari tha ipo varaikum irukka &#129303 .Be the same always &#129321. Wishing u a very happy happy birthday &#127874 .On your special day, I wish you good luck &#10024. I hope this wonderful day will fill up your &#128147 with joy and blessings &#129505.<br>\
+<br>\
+<br>\
+15.arun<br>\
+<br>\
+Hey Ragavi... Many Many Happy Returns of the day. You are one of the very first and the best people I met at CEG. I whole heartedly wish and pray for a awesome life ahead. May God bless you..<br>Happy Birthday &#127874 &#127880 &#127881<br>\
+"
+var d = dataStr.split("<br><br><br>");
+for(var i = 0; i<d.length; i++){
+  var c = d[i].split("<br><br>");
+  data.push({
+    img: c[0]+ ".jpg",
+    caption: c[0].split(".")[1],
+    desc: c[1]
+  });
+}
+
